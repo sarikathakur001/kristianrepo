4876171ddcefdc2529aaa3339d43003a3a663eaf
sarikathakur001 <sarika311dbuglab@gmail.com>
2024-10-24 03:06:30 +0530

    Update backendfile.js

--- js/backendfile.js
+++ js/backendfile.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const cors = require('cors'); // Import CORS
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const stripe = require('stripe')('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'); // Replace with your Stripe Secret Key
 
 const app = express();
 
-// Enable CORS to allow requests from the Webflow front-end
-app.use(cors({ origin: 'https://informativ-reklame-124.webflow.io' }));
+// CORS Configuration to Allow Requests from Anywhere
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Allows requests from any origin
+    res.header("Access-Control-Allow-Methods", "GET, POST"); // Allows specific methods
+    res.header("Access-Control-Allow-Headers", "Content-Type"); // Allows specific headers
+    next();
+});
 
+// Alternatively, use the CORS package for more control
+// app.use(cors({ origin: 'https://informativ-reklame-124.webflow.io' })); // Allow only Webflow domain
 
 // Middleware to parse incoming JSON requests
 app.use(bodyParser.json());
