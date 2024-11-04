94e177223f714bbe28989e256830a5f45696d570
sarikathakur001 <sarika311dbuglab@gmail.com>
2024-11-05 03:47:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+// Load environment variables from .env file
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const customerRoutes = require('./routes/customer');  // Import routes
+
+const app = express();
+
+// CORS configuration
+const corsOptions = {
+    origin: 'https://informativ-reklame-124.webflow.io',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+// Use customer routes
+app.use('/api', customerRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
