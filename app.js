87f45c50b8bf70010703d9b0f491d35c450f6051
sarikathakur001 <sarika311dbuglab@gmail.com>
2024-11-05 04:18:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const customerRoutes = require('./routes/customer');  // Import routes
+const customerRoutes = require('./js/customer');  // Import routes
 
 const app = express();
 
